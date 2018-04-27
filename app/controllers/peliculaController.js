4ebe066d2b43b357060abd10e8e5bd3e8817dbb0
alu0100825145 <alu0100825145@ull.edu.es>
2018-04-27 11:08:03 +0000

    Patrón Factory

--- app/controllers/peliculaController.js
+++ app/controllers/peliculaController.js
@@ -1,4 +1,3 @@
-
 import Pelicula        from '../models/pelicula.js'
 
 
@@ -7,8 +6,13 @@ async function main(req, res) {
     res.render('pelicula', { peliculas: peliculas })
 }
 
+function crearPelicula(req,res){
+
+	return new Pelicula({ 'nombre': req.body.nombre, 'director': req.body.director, 'genero': req.body.genero, 'puntuacion': req.body.puntuacion, 'estreno': req.body.estreno, 'duracion': req.body.duracion });
+}
+
 async function getPelicula(req, res) {
-    const pelicula = new Pelicula({ 'nombre': req.body.nombre, 'director': req.body.director, 'genero': req.body.genero, 'puntuacion': req.body.puntuacion, 'estreno': req.body.estreno, 'duracion': req.body.duracion })
+    const pelicula = crearPelicula(req,res); 
     let result = await Pelicula.findOne({'nombre': req.body.nombre})
     if (result) throw new Error('Pelicula ya existe')
     pelicula.save()
