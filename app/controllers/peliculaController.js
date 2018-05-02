3a22a7c96a356c0aa1281fc262af3e353dee3ad6
Darwin Gonzalez <alu0100812794@ull.edu.es>
2018-05-02 13:42:38 +0100

    .

--- app/controllers/peliculaController.js
+++ app/controllers/peliculaController.js
@@ -6,8 +6,14 @@ async function main(req, res) {
     res.render('pelicula', { peliculas: peliculas })
 }
 
+//----------- Begin Patrón Factory ------------------
+function crearPelicula(req,res){
+    return new Pelicula({ 'nombre': req.body.nombre, 'director': req.body.director, 'genero': req.body.genero, 'puntuacion':req.body.puntuacion, 'estreno': req.body.estreno, 'duracion': req.body.duracion });
+}
+//----------- End Patrón Factory ------------------
+
 async function getPelicula(req, res) {
-    const pelicula = new Pelicula({ 'nombre': req.body.nombre, 'director': req.body.director, 'genero': req.body.genero, 'puntuacion': req.body.puntuacion, 'estreno': req.body.estreno, 'duracion': req.body.duracion })
+    const pelicula = crearPelicula(req,res); 
     let result = await Pelicula.findOne({'nombre': req.body.nombre})
     if (result) throw new Error('Pelicula ya existe')
     pelicula.save()
